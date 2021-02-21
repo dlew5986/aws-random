0d7d8ccba1b28d0ea10ca3d8339b1df6f9f5864d
dlew5986 <19508775+dlew5986@users.noreply.github.com>
2021-02-21 18:06:20 -0500

    move into red as subdirectory

---
+++ red/terraform-state/main.tf
@@ -0,0 +1,42 @@
+terraform {
+  required_version = ">= 0.14"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-2"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "terraform-state"
+  acl    = "private"
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
