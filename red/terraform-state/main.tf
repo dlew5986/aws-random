7e318d8e2d2c8b807385c8e5f42e08c216c81549
dlew5986 <19508775+dlew5986@users.noreply.github.com>
2021-02-21 20:50:33 -0500

    add public access block to s3 bucket

--- red/terraform-state/main.tf
+++ red/terraform-state/main.tf
@@ -13,8 +13,8 @@ provider "aws" {
   region  = "us-east-2"
 }
 
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "terraform-state"
+resource "aws_s3_bucket" "red_terraform_state" {
+  bucket = "red-terraform-state"
   acl    = "private"
 
   versioning {
@@ -28,15 +28,33 @@ resource "aws_s3_bucket" "terraform_state" {
       }
     }
   }
+
+  tags = {
+    Name = "Terraform state s3 bucket for red"
+  }
+
 }
 
-resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "terraform-locks"
+resource "aws_s3_bucket_public_access_block" "red_terraform_state_public_access" {
+  bucket = aws_s3_bucket.red_terraform_state.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_dynamodb_table" "red_terraform_locks" {
+  name         = "red-terraform-state-locks"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
 
   attribute {
     name = "LockID"
     type = "S"
+  }
+
+  tags = {
+    Name = "Terraform state lock table for red"
   }
 }
